5502197ff110b3a3dbae5fbacc527b40ff5915b1
Mauro Carabotti <carabotti@puzzle.ch>
2013-01-30 19:51:39 +0100

    change to landscapeTile and add others, center in Delémont

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -4,13 +4,21 @@ define([
 
 function(L) {
 
+	var center = [47.363511,7.35706];
 	
-	var map = L.map('map').setView([51.505, -0.09], 13);
+	var map = L.map('map').setView(center, 13);
 
-	var marker = L.marker([51.5, -0.09]).addTo(map);
-	
-	L.tileLayer('http://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-		attribution: 'Map data &copy; 2013 OpenStreetMap contributors'
+	var marker = L.marker(center).addTo(map);
+
+	var landscapeTile = 'http://{s}.tile3.opencyclemap.org/landscape/{z}/{x}/{y}.png';
+
+	var cycleMapTile = 'http://{s}.tile.opencyclemap.org/cycle/{z}/{x}/{y}.png';
+
+	var openstreetmap = 'http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
+
+
+	L.tileLayer(landscapeTile, {
+		attribution: '&copy; OpenStreetMap contributors'
 	}).addTo(map);
 	
 });
