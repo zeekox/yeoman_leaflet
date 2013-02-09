8e132eef68d8dc93805e75d7de52f6305e96259b
Mauro Carabotti <carabotti@puzzle.ch>
2013-02-09 12:31:21 +0100

    working example with angularjs backend example and list/detail views.

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,13 +1,90 @@
 define([
-  'angular'
-],
+		'angular',
+		'ngResource'
+	],
 
-function(angular) {
-	var bootstrap = function(){
-		//angular.bootstrap(document);
-	};
+	function(angular, ngResource) {
 
-	return {
-		bootstrap: bootstrap
-	};
+angular.module('project', ['mongolab']).
+  config(function($routeProvider) {
+    $routeProvider.
+      when('/', {controller:ListCtrl, templateUrl:'list.html'}).
+      when('/edit/:projectId', {controller:EditCtrl, templateUrl:'detail.html'}).
+      when('/new', {controller:CreateCtrl, templateUrl:'detail.html'}).
+      otherwise({redirectTo:'/'});
+  });
+
+
+function ListCtrl($scope, Project) {
+  $scope.projects = Project.query();
+}
+
+window.ListCtrl = ListCtrl;
+
+
+function CreateCtrl($scope, $location, Project) {
+  $scope.save = function() {
+    Project.save($scope.project, function(project) {
+      $location.path('/edit/' + project._id.$oid);
+    });
+  }
+}
+window.CreateCtrl = CreateCtrl;
+
+
+function EditCtrl($scope, $location, $routeParams, Project) {
+  var self = this;
+
+  Project.get({id: $routeParams.projectId}, function(project) {
+    self.original = project;
+    $scope.project = new Project(self.original);
+  });
+
+  $scope.isClean = function() {
+    return angular.equals(self.original, $scope.project);
+  }
+
+  $scope.destroy = function() {
+    self.original.destroy(function() {
+      $location.path('/list');
+    });
+  };
+
+  $scope.save = function() {
+    $scope.project.update(function() {
+      $location.path('/');
+    });
+  };
+}
+window.EditCtrl = EditCtrl;
+
+// This is a module for cloud persistance in mongolab - https://mongolab.com
+angular.module('mongolab', ['ngResource']).
+    factory('Project', function($resource) {
+      var Project = $resource('https://api.mongolab.com/api/1/databases' +
+          '/angularjs/collections/projects/:id',
+          { apiKey: '4f847ad3e4b08a2eed5f3b54' }, {
+            update: { method: 'PUT' }
+          }
+      );
+
+      Project.prototype.update = function(cb) {
+        return Project.update({id: this._id.$oid},
+            angular.extend({}, this, {_id:undefined}), cb);
+      };
+
+      Project.prototype.destroy = function(cb) {
+        return Project.remove({id: this._id.$oid}, cb);
+      };
+
+      return Project;
+    });
+
+angular.element(document).ready(function() {
+         angular.bootstrap(document, ['mongolab','project']);
+       });
+
+
 });
+
+
--- app/scripts/main.js
+++ app/scripts/main.js
@@ -6,18 +6,24 @@ require.config({
 			'angular': {
 				exports: 'angular',
 				deps: ['jquery']
-		}},
+			},
+			'ngResource': {
+				exports: 'angular',
+				deps: ['angular']
+			}
+			
+			},
 
 		paths: {
 			jquery: '../components/jquery/jquery.min',
 			leaflet: '../components/leaflet/dist/leaflet',
 			underscore: '../components/underscore/underscore',
-			angular: '../components/angular/angular'
+			angular: '../components/angular/angular',
+			ngResource: '../components/angular-resource/angular-resource'
 		}
-
 });
 
 require(['app'], function(app) {
-		app.bootstrap();
+
 
 });
